a2f02bb2968189fe121994b82a99cac10bec1798
mayadahgithub <mayadahnaji@gmail.com>
2018-02-07 15:21:57 +0300

    add googlemap

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,16 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, trigger } from '@angular/core';
+import { NavController, Platform} from 'ionic-angular';
+
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+ } from '@ionic-native/google-maps';
+ 
 
 @Component({
   selector: 'page-home',
@@ -7,8 +18,49 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  map: GoogleMap;
+
+  constructor(public navCtrl: NavController,public platform:Platform) {
+
+    platform.ready().then(()=>{
+      this.loadmap();
+    })
 
   }
+
+    loadmap(){
+    this.map = new GoogleMap('map',{
+     'controls': {
+    'compass':true,
+    'myLocationButton':true,
+    'indoorPicker':true,
+    'zoom':true
+     },
+     'gestures':{
+       'scroll':true,
+       'tilt':true,
+       'rotate':true,
+       'zoom':true
+     },
+     'camera':{
+      target: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      },
+      zoom: 18,
+      tilt: 30,
+      'bearing':50
+     
+     }
+    });
+    this.map.one(GoogleMapsEvent.MAP_READY).then(()=>{
+      console.log('Map is ready .... :)');
+    })
+   }
+    
+  
+
+
+
 
 }
